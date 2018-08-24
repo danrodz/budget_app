a373d6efc4d258e524436bfa0dbf130758499115
danrodz <daniel.a.rodriguez@dr.com>
2018-08-24 06:40:58 -0500

    fixed migration

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20180824052757) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
-    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
